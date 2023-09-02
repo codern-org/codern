75e6f196c359fc0147e641505f4919563d92f806
riflowth <riflowth@gmail.com>
2023-09-02 15:48:10 +0700

    refactor: merge & add more workspace related

---
+++ db/migrations/000001_init.down.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `session`;
+
+DROP TABLE IF EXISTS `workspace`;
+DROP TABLE IF EXISTS `workspace_participant`;
+
+DROP TABLE IF EXISTS `assignment`;
+DROP TABLE IF EXISTS `testcase`;
+DROP TABLE IF EXISTS `submission`;
+DROP TABLE IF EXISTS `submission_result`;
