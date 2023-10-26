e32e93b3a02190565db38d50953556808e6ef9e6
riflowth <riflowth@gmail.com>
2023-10-26 16:37:42 +0700

    feat: add api info on index route

--- platform/server/controller/health.go
+++ platform/server/controller/health.go
@@ -3,14 +3,24 @@ package controller
 import (
 	"os"
 
+	"github.com/codern-org/codern/internal/config"
 	"github.com/codern-org/codern/platform/server/response"
 	"github.com/gofiber/fiber/v2"
 )
 
-type HealthController struct{}
+type HealthController struct {
+	cfg *config.Config
+}
 
-func NewHealthController() *HealthController {
-	return &HealthController{}
+func NewHealthController(cfg *config.Config) *HealthController {
+	return &HealthController{cfg: cfg}
+}
+
+func (c *HealthController) Index(ctx *fiber.Ctx) error {
+	return response.NewSuccessResponse(ctx, fiber.StatusOK, fiber.Map{
+		"name":    c.cfg.Metadata.Name,
+		"version": c.cfg.Metadata.Version,
+	})
 }
 
 func (c *HealthController) Check(ctx *fiber.Ctx) error {
