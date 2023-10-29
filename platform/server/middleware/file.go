37077f1078f5fd94e0c0db8db827c5d884dff956
riflowth <riflowth@gmail.com>
2023-10-30 00:56:54 +0700

    fix: broken route stack

--- platform/server/middleware/file.go
+++ platform/server/middleware/file.go
@@ -7,13 +7,9 @@ import (
 func NewFileMiddleware() fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
 		requestId := ctx.Locals("requestid").(string)
-		chainErr := ctx.Next()
 
-		// Manually call error handler
-		if chainErr != nil {
-			if err := ctx.App().ErrorHandler(ctx, chainErr); err != nil {
-				ctx.SendStatus(fiber.StatusInternalServerError)
-			}
+		if err := ctx.Next(); err != nil {
+			return err
 		}
 
 		ctx.Response().Header.Set("Server", "Codern File System 1.0")
